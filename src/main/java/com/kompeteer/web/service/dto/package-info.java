f05c47c09cbdc93e8bf754a79a9b26abe0d00f81
Øystein Ulseth <ulseth@gmail.com>
2017-03-12 11:25:52 +0100

    Set up Angular2 app

---
+++ src/main/java/com/kompeteer/web/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.kompeteer.web.service.dto;
---
+++ src/main/java/com/kompeteer/web/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.kompeteer.web.web.rest.vm;
