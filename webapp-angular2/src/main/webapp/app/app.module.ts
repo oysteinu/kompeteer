65737e17eddad09e40d19d4d9b9894e148ba768c
Magnus Haraldsen Amundsen <q5w@storebrand.no>
2017-03-12 19:36:57 +0100

    Working angular2 webapp

---
+++ webapp-angular2/src/main/webapp/app/app.module.ts
@@ -0,0 +1,58 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { KompeteerSharedModule, UserRouteAccessService } from './shared';
+import { KompeteerHomeModule } from './home/home.module';
+import { KompeteerAdminModule } from './admin/admin.module';
+import { KompeteerAccountModule } from './account/account.module';
+import { KompeteerEntityModule } from './entities/entity.module';
+
+import { LayoutRoutingModule } from './layouts';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        KompeteerSharedModule,
+        KompeteerHomeModule,
+        KompeteerAdminModule,
+        KompeteerAccountModule,
+        KompeteerEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        { provide: Window, useValue: window },
+        { provide: Document, useValue: document },
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class KompeteerAppModule {}
